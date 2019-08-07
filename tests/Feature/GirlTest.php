c507d052e8e10a798fc63cf42cec91d484b1942a
Aios <aioslike@gmail.com>
2019-08-07 18:08:10 +0600

    girls test

--- tests/Feature/GirlTest.php
+++ tests/Feature/GirlTest.php
@@ -9,4 +9,26 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class GirlTest extends TestCase
 {
 
+    public function testSearch()
+    {
+        $this->graphQL('
+        {
+            girls(filters: {services: [1]}, count: 1) {
+                data {
+                    first_name
+                }
+            }
+        }
+        ')->assertJson([
+            'data' => [
+                'girls' => [
+                    'data' => [
+                        [
+                            'first_name'
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
 }
