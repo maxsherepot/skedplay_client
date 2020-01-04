54799e4332aa635c8cc2b03fd8d76577d40f6eff
Aios <aioslike@gmail.com>
2020-01-04 18:01:38 +0200

    header guest links fix

--- client/components/Ui/Nav/index.jsx
+++ client/components/Ui/Nav/index.jsx
@@ -119,13 +119,15 @@ function Nav({ user, className }) {
                 />
               </svg>
             </a>
-            <Link href="/girls/add">
-              <a className="menu-icons__item hidden sm:block">
-                <button className="bg-red text-white px-5 py-2 rounded-full">
-                  Add new ad
-                </button>
-              </a>
-            </Link>
+            {user &&
+              <Link href="/girls/add">
+                <a className="menu-icons__item hidden sm:block">
+                  <button className="bg-red text-white px-5 py-2 rounded-full">
+                    Add new ad
+                  </button>
+                </a>
+              </Link>
+            }
             <Link href="/favorites/girls">
               <a className="menu-icons__item">
                 <svg
@@ -145,29 +147,39 @@ function Nav({ user, className }) {
                 {(user && user.favorites_count) || 0}
               </a>
             </Link>
-            <a className="menu-icons__item" href="#">
-              <svg
-                className="inline-block fill-red mr-1"
-                width="17"
-                height="17"
-                viewBox="0 0 17 17"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M4.33333 12.6667V12.1667C4.20073 12.1667 4.07355 12.2193 3.97978 12.3131L4.33333 12.6667ZM1 16H0.5C0.5 16.2022 0.621821 16.3846 0.808658 16.4619C0.995496 16.5393 1.21055 16.4966 1.35355 16.3536L1 16ZM2.66667 1V0.5V1ZM14.3333 1V0.5V1ZM4.6 3.5C4.32386 3.5 4.1 3.72386 4.1 4C4.1 4.27614 4.32386 4.5 4.6 4.5V3.5ZM12.4 4.5C12.6761 4.5 12.9 4.27614 12.9 4C12.9 3.72386 12.6761 3.5 12.4 3.5V4.5ZM4.6 5.9C4.32386 5.9 4.1 6.12386 4.1 6.4C4.1 6.67614 4.32386 6.9 4.6 6.9V5.9ZM10 6.9C10.2761 6.9 10.5 6.67614 10.5 6.4C10.5 6.12386 10.2761 5.9 10 5.9V6.9ZM4.6 8.3C4.32386 8.3 4.1 8.52386 4.1 8.8C4.1 9.07614 4.32386 9.3 4.6 9.3V8.3ZM11.2 9.3C11.4761 9.3 11.7 9.07614 11.7 8.8C11.7 8.52386 11.4761 8.3 11.2 8.3V9.3ZM15.5 11C15.5 11.3094 15.3771 11.6062 15.1583 11.825L15.8654 12.5321C16.2717 12.1257 16.5 11.5746 16.5 11H15.5ZM15.1583 11.825C14.9395 12.0437 14.6428 12.1667 14.3333 12.1667V13.1667C14.908 13.1667 15.4591 12.9384 15.8654 12.5321L15.1583 11.825ZM14.3333 12.1667H4.33333V13.1667H14.3333V12.1667ZM3.97978 12.3131L0.646447 15.6464L1.35355 16.3536L4.68689 13.0202L3.97978 12.3131ZM1.5 16V2.66667H0.5V16H1.5ZM1.5 2.66667C1.5 2.35725 1.62292 2.0605 1.84171 1.84171L1.1346 1.1346C0.728273 1.54093 0.5 2.09203 0.5 2.66667H1.5ZM1.84171 1.84171C2.0605 1.62292 2.35725 1.5 2.66667 1.5V0.5C2.09203 0.5 1.54093 0.728273 1.1346 1.1346L1.84171 1.84171ZM2.66667 1.5H14.3333V0.5H2.66667V1.5ZM14.3333 1.5C14.6428 1.5 14.9395 1.62292 15.1583 1.84171L15.8654 1.1346C15.4591 0.728273 14.908 0.5 14.3333 0.5V1.5ZM15.1583 1.84171C15.3771 2.0605 15.5 2.35725 15.5 2.66667H16.5C16.5 2.09203 16.2717 1.54093 15.8654 1.1346L15.1583 1.84171ZM15.5 2.66667V11H16.5V2.66667H15.5ZM4.6 4.5H12.4V3.5H4.6V4.5ZM4.6 6.9H10V5.9H4.6V6.9ZM4.6 9.3H11.2V8.3H4.6V9.3Z" />
-              </svg>
-              7
-            </a>
+            {user &&
+              <a className="menu-icons__item" href="#">
+                <svg
+                  className="inline-block fill-red mr-1"
+                  width="17"
+                  height="17"
+                  viewBox="0 0 17 17"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M4.33333 12.6667V12.1667C4.20073 12.1667 4.07355 12.2193 3.97978 12.3131L4.33333 12.6667ZM1 16H0.5C0.5 16.2022 0.621821 16.3846 0.808658 16.4619C0.995496 16.5393 1.21055 16.4966 1.35355 16.3536L1 16ZM2.66667 1V0.5V1ZM14.3333 1V0.5V1ZM4.6 3.5C4.32386 3.5 4.1 3.72386 4.1 4C4.1 4.27614 4.32386 4.5 4.6 4.5V3.5ZM12.4 4.5C12.6761 4.5 12.9 4.27614 12.9 4C12.9 3.72386 12.6761 3.5 12.4 3.5V4.5ZM4.6 5.9C4.32386 5.9 4.1 6.12386 4.1 6.4C4.1 6.67614 4.32386 6.9 4.6 6.9V5.9ZM10 6.9C10.2761 6.9 10.5 6.67614 10.5 6.4C10.5 6.12386 10.2761 5.9 10 5.9V6.9ZM4.6 8.3C4.32386 8.3 4.1 8.52386 4.1 8.8C4.1 9.07614 4.32386 9.3 4.6 9.3V8.3ZM11.2 9.3C11.4761 9.3 11.7 9.07614 11.7 8.8C11.7 8.52386 11.4761 8.3 11.2 8.3V9.3ZM15.5 11C15.5 11.3094 15.3771 11.6062 15.1583 11.825L15.8654 12.5321C16.2717 12.1257 16.5 11.5746 16.5 11H15.5ZM15.1583 11.825C14.9395 12.0437 14.6428 12.1667 14.3333 12.1667V13.1667C14.908 13.1667 15.4591 12.9384 15.8654 12.5321L15.1583 11.825ZM14.3333 12.1667H4.33333V13.1667H14.3333V12.1667ZM3.97978 12.3131L0.646447 15.6464L1.35355 16.3536L4.68689 13.0202L3.97978 12.3131ZM1.5 16V2.66667H0.5V16H1.5ZM1.5 2.66667C1.5 2.35725 1.62292 2.0605 1.84171 1.84171L1.1346 1.1346C0.728273 1.54093 0.5 2.09203 0.5 2.66667H1.5ZM1.84171 1.84171C2.0605 1.62292 2.35725 1.5 2.66667 1.5V0.5C2.09203 0.5 1.54093 0.728273 1.1346 1.1346L1.84171 1.84171ZM2.66667 1.5H14.3333V0.5H2.66667V1.5ZM14.3333 1.5C14.6428 1.5 14.9395 1.62292 15.1583 1.84171L15.8654 1.1346C15.4591 0.728273 14.908 0.5 14.3333 0.5V1.5ZM15.1583 1.84171C15.3771 2.0605 15.5 2.35725 15.5 2.66667H16.5C16.5 2.09203 16.2717 1.54093 15.8654 1.1346L15.1583 1.84171ZM15.5 2.66667V11H16.5V2.66667H15.5ZM4.6 4.5H12.4V3.5H4.6V4.5ZM4.6 6.9H10V5.9H4.6V6.9ZM4.6 9.3H11.2V8.3H4.6V9.3Z" />
+                </svg>
+                7
+              </a>
+            }
 
             {user ? (
               <UserDropdown user={user} />
             ) : (
-              <Link href="/login">
-                <a className="menu-icons__item menu-icons__item_last hidden sm:block">
-                  <ProfileSvg />
-                  Login
-                </a>
-              </Link>
+              <>
+                <Link href="/login">
+                  <a className="menu-icons__item menu-icons__item_last hidden sm:block">
+                    <ProfileSvg />
+                    Login
+                  </a>
+                </Link>
+
+                <Link href="/register">
+                  <a className="menu-icons__item menu-icons__item_last hidden sm:block ml-3">
+                    Sign up
+                  </a>
+                </Link>
+              </>
             )}
 
             <button
@@ -243,11 +255,13 @@ function Nav({ user, className }) {
               </Link>
             </li>
           </ul>
-          <Link href="/girls/add">
-            <a>
-              <Button className="w-full text-2xl mt-1">Add new ad</Button>
-            </a>
-          </Link>
+          {user &&
+            <Link href="/girls/add">
+              <a>
+                <Button className="w-full text-2xl mt-1">Add new ad</Button>
+              </a>
+            </Link>
+          }
           {user ? (
             <Link href="/profile/[id]" as={`/profile/${user.id}`}>
               <a className="block text-center transition tracking-tighter text-white hover:text-red text-2xl font-medium my-8">
@@ -255,11 +269,18 @@ function Nav({ user, className }) {
               </a>
             </Link>
           ) : (
-            <Link href="/login">
-              <a className="block text-center transition tracking-tighter text-white hover:text-red text-2xl font-medium my-8">
-                Login
-              </a>
-            </Link>
+            <>
+              <Link href="/login">
+                <a className="block text-center transition tracking-tighter text-white hover:text-red text-2xl font-medium my-8">
+                  Login
+                </a>
+              </Link>
+              <Link href="/register">
+                <a className="block text-center transition tracking-tighter text-white hover:text-red text-2xl font-medium my-8">
+                  Sign up
+                </a>
+              </Link>
+            </>
           )}
           <div className="locales">
             <a className="active" href="#ru">
