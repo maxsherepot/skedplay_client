75c62c0eb922d1c220a7cf3af7b50c6dcedaf4ff
Aios <aioslike@gmail.com>
2020-03-02 10:32:08 +0200

    user dropdown - plans link

--- client/components/user/UserDropdown.jsx
+++ client/components/user/UserDropdown.jsx
@@ -57,9 +57,11 @@ const UserDropdown = ({ user }) => {
             </div>
             <div className="flex items-center">
               <div className="mr-2">{t('common.plan')}: {t('common.free')}</div>
-              <div className="bg-transparent hover:bg-pink hover:text-white border border-red rounded-full text-xs ml-4 px-2-5 leading-loose cursor-pointer">
-                {t('common.upgrade')}
-              </div>
+              <Link href="/plans">
+                <div className="bg-transparent hover:bg-pink hover:text-white border border-red rounded-full text-xs ml-4 px-2-5 leading-loose cursor-pointer">
+                  {t('common.upgrade')}
+                </div>
+              </Link>
             </div>
           </div>
         </div>
