82b221bcb6b52eab6dfc0e9ae90eb744b5fd79cc
Aios <aioslike@gmail.com>
2020-05-27 16:48:54 +0300

    employee editing bug fix

--- client/rules/employee.js
+++ client/rules/employee.js
@@ -2,15 +2,15 @@ import * as Yup from "yup";
 
 export const employeeRules = Yup.object().shape({
     name: Yup.string().required(),
-    birthday: Yup.string().nullable(),
-    gender: Yup.string().required().nullable(),
+    // birthday: Yup.string().nullable(),
+    // gender: Yup.string().required().nullable(),
     race_type_id: Yup.string().required().nullable(),
     type: Yup.string().nullable(),
     description: Yup.string().required().nullable(),
     index: Yup.string().nullable(),
-    city: Yup.string().nullable(),
+    // city: Yup.string().nullable(),
     address: Yup.string().required(),
-    phone: Yup.string().nullable(),
+    // phone: Yup.string().nullable(),
     email: Yup.string().nullable(),
     website: Yup.string().nullable()
 });
