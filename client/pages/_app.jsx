38979914dbf4e42222a12f46fdb8de4c712aee9a
Aios <aioslike@gmail.com>
2020-06-11 18:27:14 +0300

    hreflang tags

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -6,8 +6,39 @@ import {MainLayout} from 'layouts'
 import "styles/style.scss";
 import { appWithTranslation } from 'lib/i18n';
 import slug from 'slug';
+import {useRouter} from "next/router";
+import {useTranslation} from "react-i18next";
+import { NextSeo } from 'next-seo';
 
 slug.defaults.mode = 'rfc3986';
+
+const LangTags = () => {
+  const {asPath} = useRouter();
+  const {i18n} = useTranslation();
+
+  const tags = i18n.options.allLanguages.map(lang => {
+    let path = asPath;
+
+    for (let i in i18n.options.otherLanguages) {
+      path = path.replace(`${i18n.options.otherLanguages[i]}/`, '/');
+    }
+
+    if (lang === 'de' && path === '/') {
+      path = '';
+    }
+
+    return {
+      hrefLang: lang,
+      href: `${process.env.APP_URL}${lang === 'de' ? '' : `/${lang}`}${path}`
+    };
+  });
+
+  return (
+    <NextSeo
+      languageAlternates={tags}
+    />
+  );
+};
 
 class MyApp extends App {
   static displayName = "MyApp";
@@ -20,6 +51,7 @@ class MyApp extends App {
 
       return (
         <ApolloProvider client={apolloClient}>
+          <LangTags/>
           {getLayout(<Component {...pageProps} />, pageProps)}
         </ApolloProvider>
       )
