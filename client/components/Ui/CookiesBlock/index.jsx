c264ecef0ba70d709fa0410031c786846fe78c51
maxsherepot <48758609+maxsherepot@users.noreply.github.com>
2020-11-04 22:39:11 +0200

    Зроблені правильні посилання в вікні Cookies

--- client/components/Ui/CookiesBlock/index.jsx
+++ client/components/Ui/CookiesBlock/index.jsx
@@ -1,13 +1,13 @@
 import Cookies from 'js-cookie';
-import {useState} from "react";
-import {useTranslation, Trans} from "react-i18next";
-import {Button} from "UI";
+import { useState } from "react";
+import { useTranslation, Trans } from "react-i18next";
+import { Button } from "UI";
 import Link from 'components/SlashedLink'
 
 const CookiesBlock = () => {
   const [cookiesAllowed, setCookiesAllowed] = useState(Cookies.get('cookies_allowed') === 'true');
 
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
   if (typeof document === 'undefined') {
     return '';
@@ -24,22 +24,22 @@ const CookiesBlock = () => {
   }
 
   return (
-    <div className="fixed bottom-0 py-10 px-4 items-center bg-light-grey-transparent w-full flex justify-center text-light-grey z-10" style={{backgroundColor: "rgba(27,27,27,.9)"}}>
+    <div className="fixed bottom-0 py-10 px-4 items-center bg-light-grey-transparent w-full flex justify-center text-light-grey z-10" style={{ backgroundColor: "rgba(27,27,27,.9)" }}>
       <div className="flex flex-col sm:flex-row sm:justify-center sm:items-center">
-          <div style={{maxWidth: 550}}>
-              <Trans i18nKey="cookies.text">
-                This site uses cookies to offer you a better browsing experience. Find out more on&nbsp;
-                <a href="#" className="underline">how we use cookies</a>&nbsp;
-                and&nbsp;
-                <a href="#" className="underline">how you can change your settings</a>.
+        <div style={{ maxWidth: 550 }}>
+          <Trans i18nKey="cookies.text">
+            This site uses cookies to offer you a better browsing experience. Find out more on&nbsp;
+                <a href="/helpcenter/privacy-policy/" className="underline">how we use cookies</a>&nbsp;
+            and&nbsp;
+                <a href="/helpcenter/terms-and-conditions/" className="underline">how you can change your settings</a>.
               </Trans>
-          </div>
+        </div>
         <div className="w-full sm:w-auto flex justify-end">
-            <div className="w-full mt-1 mr-5" style={{minWidth: 120, maxWidth: 120}}>
-                <Button className="ml-4 px-2 text-light-grey1 w-full" level="primary" size="sm" onClick={allowCookies}>
-                  {t('common.okay')}
-                </Button>
-            </div>
+          <div className="w-full mt-1 mr-5" style={{ minWidth: 120, maxWidth: 120 }}>
+            <Button className="ml-4 px-2 text-light-grey1 w-full" level="primary" size="sm" onClick={allowCookies}>
+              {t('common.okay')}
+            </Button>
+          </div>
         </div>
       </div>
     </div>
